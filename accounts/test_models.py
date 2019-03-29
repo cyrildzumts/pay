8f81f500467536bd05c6c359f45a5530a792a894
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-03-29 11:30:23 +0100

    updated accounts Account unit test

--- accounts/test_models.py
+++ accounts/test_models.py
@@ -105,16 +105,16 @@ class AccountTestCase(TestCase):
         #cls.user1 = User.objects.create(**user1)
         #cls.user2 = User.objects.create(**user2)
         #cls.user3 = User.objects.create(**user3)
-        cls.users = [User(**userdata) for userdata in users]
-        User.objects.bulk_create(cls.users)
+        cls.users = [User.objects.create(**userdata) for userdata in users]
+
 
     def test_account_creation(self):
         """ We should have 3 users in the database"""
         account_set = Account.objects.all()
         self.assertEqual(first=account_set.count() , second=3, msg="There are 3 users account in the database")
-        self.assertEqual(first=account_set.filter(user__username=user1['username']).count(), second=1,msg="any user account in the database must be unique")
-        self.assertEqual(first=account_set.filter(user__username=user2['username']).count(), second=1,msg="any user account in the database must be unique")
-        self.assertEqual(first=account_set.filter(user__username=user3['username']).count(), second=1,msg="any user account in the database must be unique")
+        self.assertEqual(first=account_set.filter(user__username=self.users[0]['username']).count(), second=1,msg="any user account in the database must be unique")
+        self.assertEqual(first=account_set.filter(user__username=self.users[1]['username']).count(), second=1,msg="any user account in the database must be unique")
+        self.assertEqual(first=account_set.filter(user__username=self.users[2]['username']).count(), second=1,msg="any user account in the database must be unique")
 
 
     def test_account_default_state(self):
@@ -128,9 +128,9 @@ class AccountTestCase(TestCase):
 
     def test_update_account(self):
         account_set = Account.objects.all()
-        account_set.filter(user__username=user1['username']).update(**account1)
-        account_set.filter(user__username=user2['username']).update(**account2)
-        account_set.filter(user__username=user3['username']).update(**account3)
+        account_set.filter(user__username=self.users[0]['username']).update(**account1)
+        account_set.filter(user__username=self.users[1]['username']).update(**account2)
+        account_set.filter(user__username=self.users[2]['username']).update(**account3)
 
         #account_set = Account.objects.all()
         self.assertTrue(account_set.filter(account_type='P').count() == 2)
@@ -148,8 +148,7 @@ class PolicyTestCase(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.policies_obj = [Policy(**policy) for policy in policies]
-        cls.policy = Policy.objects.bulk_create(cls.policies_obj)
+        cls.policies = [Policy.objects.create(**policy) for policy in policies]
     
     def test_policy_creation(self):
         self.assertTrue(Policy.objects.count() == 4, msg="There should be 4 Policy Entries in the database")
