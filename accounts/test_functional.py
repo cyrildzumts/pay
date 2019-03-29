87774e34761d6b315ea045a6e1231947318bb73d
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-03-29 12:17:36 +0100

    updated accounts Account unit test

--- accounts/test_functional.py
+++ accounts/test_functional.py
@@ -73,5 +73,5 @@ class AccountServiceTest(TestCase):
 
     def test_create_account(self):
 
-        created = AccountService(accountdata=account1, userdata=user1)
+        created = AccountService.create_account(accountdata=account1, userdata=user1)
         self.assertTrue(created == (Account.objects.filter(user__username=user1['username']).filter(**account1).count() == 1 ))
