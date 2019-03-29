b62895a993585624b36b87f3b9edfae47907afc6
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-03-29 15:25:59 +0100

    updated accounts Account unit test

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -6,32 +6,32 @@ from accounts.views import user_account, edit_account, login, logout, register, 
 
 class AccoutPage(TestCase):
     def test_account_root_url_resolve(self):
-        found = resolve('account/')
+        found = resolve('/account/')
         self.assertEqual(found.func, user_account)
 
     def test_edit_account_url(self):
-        found = resolve('account/edit_account')
+        found = resolve('/account/edit_account')
         self.assertEqual(found.func, edit_account)
 
 
     def test_account_transactions_url(self):
-        found = resolve('account/transactions')
+        found = resolve('/account/transactions')
         self.assertEqual(found.func, transactions)
     
     def test_account_services_url(self):
-        found = resolve('account/services')
+        found = resolve('/account/services')
         self.assertEqual(found.func, services)
     
 
     def test_login_url(self):
-        found = resolve('account/login')
+        found = resolve('/account/login')
         self.assertEqual(found.func, login)
     
     def test_logout_url(self):
-        found = resolve('account/logout')
+        found = resolve('/account/logout')
         self.assertEqual(found.func, logout)
 
     def test_register_url(self):
-        found = resolve('account/register')
+        found = resolve('/account/register')
         self.assertEqual(found.func, register)
     
