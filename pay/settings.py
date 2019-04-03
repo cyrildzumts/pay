d7e48e1288de81c60051490ebaa228e18c658a86
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-04-03 10:50:10 +0200

    added first initialization code

--- pay/settings.py
+++ pay/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,7 +130,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
+LANGUAGES = (
+    ('en',_('English')),
+    ('fr',_('French')),
+)
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'conf/locale'),
+)
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
