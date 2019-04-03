ab39d427c82c3ffd2fdcde1c2587b3ffda88ee1d
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-04-03 13:17:43 +0200

    added initial english translation as for internationalization

--- pay/settings.py
+++ pay/settings.py
@@ -129,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'fr'
+LANGUAGE_CODE = 'en'
 LANGUAGES = (
     ('en',_('English')),
     ('fr',_('French')),
