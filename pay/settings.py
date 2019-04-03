d07aa2bc0345b13a276428ee7986e14f2e44803d
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-04-03 12:58:31 +0200

    added initial english translation as for internationalization

--- pay/settings.py
+++ pay/settings.py
@@ -129,14 +129,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 LANGUAGES = (
     ('en',_('English')),
     ('fr',_('French')),
 )
-LOCALE_PATHS = (
-    os.path.join(BASE_DIR, 'conf/locale'),
-)
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'conf/locale')
+]
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
