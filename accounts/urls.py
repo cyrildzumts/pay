703a071dc5e64f78cf635eae817eef3925c69fa0
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-04-30 13:34:50 +0200

    finished password reset code

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from django.urls import path
+from django.urls import path, reverse_lazy
 # from demosite import settings
 from django.contrib.auth import views as auth_views
 from accounts import views
@@ -14,7 +14,7 @@ urlpatterns = [
     path('password_change/', views.password_change_views, name='password_change'),
     path('password_change_done/', views.password_change_done_views, name='password_change_done'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('services/', views.services, name='services'),
