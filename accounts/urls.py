39bdf16763f7a17a8b50d6fd391b9c8fc5a691f1
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-04-30 13:38:23 +0200

    finished password reset code

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('password_change/', views.password_change_views, name='password_change'),
     path('password_change_done/', views.password_change_done_views, name='password_change_done'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done')), name='password_reset'),
     path('password_reset_done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
