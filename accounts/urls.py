f617c18b36c13de3562a7401bda2a862fdd21176
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-04-30 15:03:40 +0200

    fixed reset confirm view: success_url redirect is now provided

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('password_change_done/', views.password_change_done_views, name='password_change_done'),
     path('password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done')), name='password_reset'),
     path('password_reset_done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('accounts:password_reset_complete')), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('services/', views.services, name='services'),
     path('transactions/', views.transactions, name='transactions'),
