34894e96471274bdc0f04e72ccf26b12d560d9f4
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-07-22 03:30:14 +0200

    added rabiitmq routing settings.

--- pay/celery.py
+++ pay/celery.py
@@ -1,8 +1,17 @@
 import os
+from kombu import Exchange, Queue
 from pay import settings
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pay.settings')
 app = Celery(settings.SITE_NAME)
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.task_queues = (
+    Queue('pay-default', Exchange('pay-default'), routing_key='pay-default'),
+    Queue('pay-outgoing-mails', Exchange('pay-mail'), routing_key='pay.mail.outgoing'),
+    Queue('pay-ident', Exchange('pay-ident'), routing_key='pay.identification'),
+)
+app.conf.task_default_queue = 'pay-default'
+app.conf.task_default_exchange_type = 'direct'
+app.conf.task_default_routing_key = 'pay.default'
 app.autodiscover_tasks()
