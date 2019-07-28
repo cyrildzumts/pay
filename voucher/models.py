640d6bb8b210fa33ae74bb0e7c612fedd49ab1a2
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-07-28 18:56:32 +0200

    VOUCHER : fixed Voucher get_absolute_url

--- voucher/models.py
+++ voucher/models.py
@@ -24,7 +24,7 @@ class Voucher(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("voucher_details", kwargs={"pk": self.pk})
+        return reverse("voucher:voucher_details", kwargs={"pk": self.pk})
 
 
 class SoldVoucher(models.Model):
