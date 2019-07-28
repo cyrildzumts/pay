7e8e418645327ceee017e8c5eb5a28004d418e18
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-07-28 18:50:11 +0200

    VOUCHER : fixed arguments passing to voucher generation task

--- voucher/tasks.py
+++ voucher/tasks.py
@@ -9,14 +9,23 @@ logger = logging.getLogger(__name__)
 batch_size = 15
 
 @shared_task
-def generate_voucher(name='PAY-STANDARD', amount=2500, number=100):
+def generate_voucher(context={}):
+    
+    name='PAY-STANDARD'
+    amount=2500
+    number=100
+    if context :
+        name = context.get('name', name)
+        amount = context.get('amount', amount)
+        number = context.get('number', number)
+
     logger.info("Starting generating %s  %s vouchers card with credit of %s", number, name, amount)
     kwargs = {
         'name' : name,
         'amount': amount
     }
     voucher_codes = [ voucher_service.voucher_generate() for i in range(number)]
-    
+    logger.info("Gener")
     vouchers = (Voucher(name=name, amount=amount, voucher_code=code) for code in voucher_codes)
     while True:
         batch = list(islice(vouchers, batch_size))
