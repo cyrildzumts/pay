9a4434a56782c5fca919c931c173682b600bc5f2
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-08-09 00:13:22 +0200

    Added first dashboard implementation views

---
+++ dashboard/urls.py
@@ -0,0 +1,34 @@
+from django.conf.urls import url, include
+from django.urls import path, reverse_lazy
+# from demosite import settings
+from dashboard import views
+
+app_name = 'dashboard'
+urlpatterns = [
+    path('', views.dashboard, name='home'),
+    path('available_services/', views.available_services, name='available_services'),
+    path('available_service_details/<int:pk>/', views.available_service_details, name='available_service_details'),
+    path('cases/', views.cases, name='cases'),
+    path('cases/<int:pk>/', views.case_details, name='case_details'),
+    path('idcards/', views.idcards, name='idcards'),
+    path('idcards/<int:pk>/', views.idcard_details, name='idcard_details'),
+    path('reductions/', views.reductions, name='reductions'),
+    path('reductions/<int:pk>/', views.reduction_details, name='reduction_details'),
+    path('services/', views.services, name='services'),
+    path('services/<int:pk>/', views.service_details, name='service_details'),
+    path('transactions/', views.transactions, name='transactions'),
+    path('transfers/', views.transfers, name='transfers'),
+    path('transactions/<int:pk>/', views.transaction_details, name='transaction_details'),
+    path('transfer/<int:pk>/', views.transfer_details, name='transfer_details'),
+    path('upload_idcard/', views.upload_idcard, name='upload_idcard'),
+    path('idcard/update/<int:pk>/', views.update_idcard, name='idcard_update'),
+    path('payments/', views.payments, name='payments'),
+    path('payments/<int:pk>/', views.payment_details, name='payment_details'),
+    path('policies/', views.policies, name='policies'),
+    path('policies/<int:pk>/', views.policy_details, name='policy_details'),
+    path('recharge/', views.recharge, name='recharge'),
+    path('service_categories/', views.service_categories, name='service_categories'),
+    path('service_categories/<int:pk>/', views.service_category_details, name='service_categories_details'),
+    
+
+]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,157 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, redirect
+from django.utils.translation import gettext as _
+from django.contrib.auth.models import User
+from django.db.models import F, Q
+from pay import utils, settings
 # Create your views here.
+
+
+
+def dashboard(request):
+    pass
+
+
+def service_details(request, pk=None):
+    context = {}
+    model = utils.get_model('accounts', 'Service')
+    user_services = model.objects.filter(Q(operator=request.user) | Q(customer=request.user) )
+    service = get_object_or_404(user_services, pk=pk)
+    template_name = "dashboard/service_details.html"
+    page_title = "Service Details - " + settings.SITE_NAME
+    context['page_title'] = page_title
+    context['site_name'] = settings.SITE_NAME
+    context['service'] = service
+    return render(request,template_name, context)
+
+@login_required
+def services(request):
+    context = {}
+    model = utils.get_model('accounts', 'Service')
+    services = model.objects.select_related('category').all()
+    template_name = "dashboard/service_list.html"
+    page_title = _("Dashboard Services") + " - " + settings.SITE_NAME
+    context['page_title'] = page_title
+    context['site_name'] = settings.SITE_NAME
+    context['services'] = services
+    return render(request,template_name, context)
+
+@login_required
+def available_services(request):
+    context = {}
+    model = utils.get_model('accounts', 'AvailableService')
+    available_services = model.objects.all()
+    template_name = "dashboard/available_service_list.html"
+    page_title = "Available Services - " + settings.SITE_NAME
+    context['page_title'] = page_title
+    context['site_name'] = settings.SITE_NAME
+    context['available_services'] = available_services
+    return render(request,template_name, context)
+
+def available_service_update(request, pk=None):
+    pass
+
+@login_required
+def available_service_create(request):
+    pass
+
+@login_required
+def available_service_remove(request, pk=None):
+    pass
+
+@login_required
+def available_service_details(request, pk=None):
+    context = {}
+    model = utils.get_model('accounts', 'AvailableService')
+    service= get_object_or_404(model, pk=pk)
+    template_name = "dashboard/available_service_details.html"
+    page_title = "Available Service Details - " + settings.SITE_NAME
+    context['page_title'] = page_title
+    context['site_name'] = settings.SITE_NAME
+    context['service'] = service
+    return render(request,template_name, context)
+
+@login_required
+def category_services(request):
+    context = {}
+    model = utils.get_model('accounts', 'ServiceCategory')
+    categories = model.objects.filter(is_active=True)
+    template_name = "dashboard/service_category_list.html"
+    page_title = "Service Categories - " + settings.SITE_NAME
+    context['page_title'] = page_title
+    context['site_name'] = settings.SITE_NAME
+    context['categories'] = categories
+    return render(request,template_name, context)
+
+
+@login_required
+def category_service_update(request, pk=None):
+    pass
+
+
+@login_required
+def category_service_remove(request, pk=None):
+    pass
+
+
+@login_required
+def category_service_create(request):
+    pass
+
+
+@login_required
+def category_service_details(request, pk=None):
+    context = {}
+    model = utils.get_model('accounts', 'ServiceCategory')
+    category = get_object_or_404(model, pk=pk)
+    template_name = "dashboard/service_category_details.html"
+    page_title = "Service Category Details - " + settings.SITE_NAME
+    context['page_title'] = page_title
+    context['site_name'] = settings.SITE_NAME
+    context['category'] = category
+    return render(request,template_name, context)
+
+
+@login_required
+def policies(request):
+    context = {}
+    model = utils.get_model(app_name='accounts', modelName='Policy')
+    #current_account = Account.objects.get(user=request.user)
+    current_policies = model.objects.all()
+    template_name = "dashboard/policy_list.html"
+    page_title = "Policies - " + settings.SITE_NAME
+    context['page_title'] = page_title
+    context['site_name'] = settings.SITE_NAME
+    context['policies'] = current_policies
+    return render(request,template_name, context)
+
+
+@login_required
+def policy_update(request, pk=None):
+    pass
+
+
+
+@login_required
+def policy_remove(request, pk=None):
+    pass
+
+
+
+@login_required
+def policy_create(request):
+    pass
+
+
+@login_required
+def policy_details(request, pk=None):
+    context = {}
+    model = utils.get_model(app_name='accounts', modelName='Policy')
+    #current_account = Account.objects.get(user=request.user)
+    policy = get_object_or_404(model, pk=pk)
+    template_name = "dashboard/policy_details.html"
+    page_title = "Policy Details - " + settings.SITE_NAME
+    context['page_title'] = page_title
+    context['site_name'] = settings.SITE_NAME
+    context['policy'] = policy
+    return render(request,template_name, context)
