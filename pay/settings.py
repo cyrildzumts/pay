c3eac1c30d73b0ce3d2c4611d6f7b638475d81ae
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-10-14 00:57:42 +0200

    update

--- pay/settings.py
+++ pay/settings.py
@@ -88,11 +88,11 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
+    #'DEFAULT_PERMISSION_CLASSES': [
         #'rest_framework.permissions.IsAdminUser',
-        'rest_framework.permission.IsAuthenticated',
+        #'rest_framework.permission.IsAuthenticated',
         #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ]
+    #]
 }
 
 
