81a941123b52fb316113c70c9c8720b54abe2074
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-10-27 14:35:56 +0100

    minor fix on rest api

--- pay/settings.py
+++ pay/settings.py
@@ -91,7 +91,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         #'rest_framework.permissions.IsAdminUser',
-        'rest_framework.permission.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         'rest_framework.authentication.TokenAuthentication',
     ]
