6a3a1b09761de98fdb7ec558895e7fbfb4800e81
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-12-08 21:08:42 +0100

    minor update

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,87 +1,91 @@
 from django.db import models
 
+
+DASHBOARD_VIEW_PERM                 = 'can_view_dashboard'
+TOKEN_GENERATE_PERM                 = 'can_generate_token'
+ACCOUNT_VIEW_PERM                     = 'can_view_account'
+ACCOUNT_CREATE_PERM                   = 'can_add_account'
+ACCOUNT_CHANGE_PERM                   = 'can_change_account'
+ACCOUNT_DELETE_PERM                   = 'can_delete_account'
+
+CASE_ISSUE_VIEW_PERM                     = 'can_view_claim'
+CASE_ISSUE_CREATE_PERM                   = 'can_add_claim'
+CASE_ISSUE_CHANGE_PERM                   = 'can_change_claim'
+CASE_ISSUE_DELETE_PERM                   = 'can_delete_claim'
+CASE_ISSUE_CLOSE_PERM                    = 'can_close_claim'
+
+GROUP_VIEW_PERM                     = 'can_view_group'
+GROUP_ADD_PERM                   = 'can_add_group'
+GROUP_CHANGE_PERM                   = 'can_change_group'
+GROUP_DELETE_PERM                   = 'can_delete_group'
+
+IDCARD_VIEW_PERM                    = 'can_view_idcard'
+IDCARD_CREATE_PERM                  = 'can_add_idcard'
+IDCARD_CHANGE_PERM                  = 'can_change_idcard'
+IDCARD_DELETE_PERM                  = 'can_delete_idcard'
+
+VOUCHER_VIEW_PERM                   = 'can_view_voucher'
+VOUCHER_ADD_PERM                 = 'can_add_voucher'
+VOUCHER_CHANGE_PERM                 = 'can_change_voucher'
+VOUCHER_DELETE_PERM                 = 'can_delete_voucher'
+VOUCHER_ACTIVATE_PERM               = 'can_activate_voucher'
+VOUCHER_RECHARGE_ACCOUNT_PERM       = 'can_recharge_account_voucher'
+
+PAYMENT_VIEW_PERM                   = 'can_view_payment'
+PAYMENT_CREATE_PERM                 = 'can_add_payment'
+PAYMENT_CHANGE_PERM                 = 'can_change_payment'
+PAYMENT_DELETE_PERM                 = 'can_delete_payment'
+
+POLICY_VIEW_PERM                    = 'can_view_policy'
+POLICY_CREATE_PERM                  = 'can_add_policy'
+POLICY_CHANGE_PERM                  = 'can_change_policy'
+POLICY_DELETE_PERM                  = 'can_delete_policy'
+
+CATEGORY_VIEW_PERM                  = 'can_view_category'
+CATEGORY_CREATE_PERM                = 'can_add_category'
+CATEGORY_CHANGE_PERM                = 'can_change_category'
+CATEGORY_DELETE_PERM                = 'can_delete_category'
+
+AVAILABLE_SERVICE_VIEW_PERM         = 'can_view_available_service'
+AVAILABLE_SERVICE_CREATE_PERM       = 'can_add_available_service'
+AVAILABLE_SERVICE_CHANGE_PERM       = 'can_change_available_service'
+AVAILABLE_SERVICE_DELETE_PERM       = 'can_delete_available_service'
+
+SERVICE_VIEW_PERM                   = 'can_view_service'
+SERVICE_CREATE_PERM                 = 'can_add_service'
+SERVICE_CHANGE_PERM                 = 'can_change_service'
+SERVICE_DELETE_PERM                 = 'can_delete_service'
+
+TRANSFER_VIEW_PERM                  = 'can_view_transfer'
+TRANSFER_CREATE_PERM                = 'can_add_transfer'
+TRANSFER_CHANGE_PERM                = 'can_change_transfer'
+TRANSFER_DELETE_PERM                = 'can_delete_transfer'
+
+REDUCTION_VIEW_PERM                  = 'can_view_reduction'
+REDUCTION_CREATE_PERM                = 'can_add_reduction'
+REDUCTION_CHANGE_PERM                = 'can_change_reduction'
+REDUCTION_DELETE_PERM                = 'can_delete_reduction'
+
+
 # Create your models here.
 class AccessPermissions(models.Model):
-    DASHBOARD_VIEW_PERM                 = 'can_view_dashboard'
-    TOKEN_GENERATE_PERM                 = 'can_generate_token'
-    ACCOUNT_VIEW_PERM                     = 'can_view_account'
-    ACCOUNT_CREATE_PERM                   = 'can_add_account'
-    ACCOUNT_CHANGE_PERM                   = 'can_change_account'
-    ACCOUNT_DELETE_PERM                   = 'can_delete_account'
-
-    CASE_ISSUE_VIEW_PERM                     = 'can_view_claim'
-    CASE_ISSUE_CREATE_PERM                   = 'can_add_claim'
-    CASE_ISSUE_CHANGE_PERM                   = 'can_change_claim'
-    CASE_ISSUE_DELETE_PERM                   = 'can_delete_claim'
-    CASE_ISSUE_CLOSE_PERM                    = 'can_close_claim'
-
-    GROUP_VIEW_PERM                     = 'can_view_group'
-    GROUP_ADD_PERM                   = 'can_add_group'
-    GROUP_CHANGE_PERM                   = 'can_change_group'
-    GROUP_DELETE_PERM                   = 'can_delete_group'
-
-    IDCARD_VIEW_PERM                    = 'can_view_idcard'
-    IDCARD_CREATE_PERM                  = 'can_add_idcard'
-    IDCARD_CHANGE_PERM                  = 'can_change_idcard'
-    IDCARD_DELETE_PERM                  = 'can_delete_idcard'
-
-    VOUCHER_VIEW_PERM                   = 'can_view_voucher'
-    VOUCHER_CREATE_PERM                 = 'can_add_voucher'
-    VOUCHER_CHANGE_PERM                 = 'can_change_voucher'
-    VOUCHER_DELETE_PERM                 = 'can_delete_voucher'
-    VOUCHER_ACTIVATE_PERM               = 'can_activate_voucher'
-    VOUCHER_RECHARGE_ACCOUNT_PERM       = 'can_recharge_account_voucher'
-
-    PAYMENT_VIEW_PERM                   = 'can_view_payment'
-    PAYMENT_CREATE_PERM                 = 'can_add_payment'
-    PAYMENT_CHANGE_PERM                 = 'can_change_payment'
-    PAYMENT_DELETE_PERM                 = 'can_delete_payment'
-
-    POLICY_VIEW_PERM                    = 'can_view_policy'
-    POLICY_CREATE_PERM                  = 'can_add_policy'
-    POLICY_CHANGE_PERM                  = 'can_change_policy'
-    POLICY_DELETE_PERM                  = 'can_delete_policy'
-
-    CATEGORY_VIEW_PERM                  = 'can_view_category'
-    CATEGORY_CREATE_PERM                = 'can_add_category'
-    CATEGORY_CHANGE_PERM                = 'can_change_category'
-    CATEGORY_DELETE_PERM                = 'can_delete_category'
-
-    AVAILABLE_SERVICE_VIEW_PERM         = 'can_view_available_service'
-    AVAILABLE_SERVICE_CREATE_PERM       = 'can_add_available_service'
-    AVAILABLE_SERVICE_CHANGE_PERM       = 'can_change_available_service'
-    AVAILABLE_SERVICE_DELETE_PERM       = 'can_delete_available_service'
-
-    SERVICE_VIEW_PERM                   = 'can_view_service'
-    SERVICE_CREATE_PERM                 = 'can_add_service'
-    SERVICE_CHANGE_PERM                 = 'can_change_service'
-    SERVICE_DELETE_PERM                 = 'can_delete_service'
-
-    TRANSFER_VIEW_PERM                  = 'can_view_transfer'
-    TRANSFER_CREATE_PERM                = 'can_add_transfer'
-    TRANSFER_CHANGE_PERM                = 'can_change_transfer'
-    TRANSFER_DELETE_PERM                = 'can_delete_transfer'
-
-    REDUCTION_VIEW_PERM                  = 'can_view_reduction'
-    REDUCTION_CREATE_PERM                = 'can_add_reduction'
-    REDUCTION_CHANGE_PERM                = 'can_change_reduction'
-    REDUCTION_DELETE_PERM                = 'can_delete_reduction'
+   
 
 
     class Meta:
         managed = False
         permissions = {
-            (AccessPermissions.DASHBOARD_VIEW_PERM, 'Can view Dashboard'),
-            (AccessPermissions.GROUP_ADD_PERM, 'Can create a Group'),
-            (AccessPermissions.GROUP_CHANGE_PERM, 'Can change a Group'),
-            (AccessPermissions.GROUP_DELETE_PERM, 'Can delete a Group'),
-            (AccessPermissions.GROUP_VIEW_PERM, 'Can view Group'),
-            (AccessPermissions.VOUCHER_VIEW_PERM, 'Can view Voucher'),
-            (AccessPermissions.VOUCHER_DELETE_PERM, 'Can delete Voucher'),
-            (AccessPermissions.VOUCHER_CHANGE_PERM, 'Can change Voucher'),
-            (AccessPermissions.VOUCHER_ADD_PERM, 'Can create Voucher'),
-            (AccessPermissions.TOKEN_GENERATE_PERM, 'Can generate Token'),
-            (AccessPermissions.VOUCHER_ACTIVATE_PERM, 'Can activate Voucher'),
-            (AccessPermissions.VOUCHER_RECHARGE_ACCOUNT_PERM, 'Can recharge an Account'),
+            (DASHBOARD_VIEW_PERM, 'Can view Dashboard'),
+            (GROUP_ADD_PERM, 'Can create a Group'),
+            (GROUP_CHANGE_PERM, 'Can change a Group'),
+            (GROUP_DELETE_PERM, 'Can delete a Group'),
+            (GROUP_VIEW_PERM, 'Can view Group'),
+            (VOUCHER_VIEW_PERM, 'Can view Voucher'),
+            (VOUCHER_DELETE_PERM, 'Can delete Voucher'),
+            (VOUCHER_CHANGE_PERM, 'Can change Voucher'),
+            (VOUCHER_ADD_PERM, 'Can create Voucher'),
+            (TOKEN_GENERATE_PERM, 'Can generate Token'),
+            (VOUCHER_ACTIVATE_PERM, 'Can activate Voucher'),
+            (VOUCHER_RECHARGE_ACCOUNT_PERM, 'Can recharge an Account')
 
         }
--- dashboard/permissions.py
+++ dashboard/permissions.py
@@ -1,4 +1,4 @@
-from dashboard.models import AccessPermissions
+import dashboard.models as Constants
 
 
 
@@ -17,210 +17,210 @@ class PermissionManager :
 
     @staticmethod
     def user_can_generate_token(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.TOKEN_GENERATE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.TOKEN_GENERATE_PERM)
 
     @staticmethod
     def user_can_access_dashboard(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.DASHBOARD_VIEW_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.DASHBOARD_VIEW_PERM)
 
     ## ACCOUNT PERMISSION
     @staticmethod
     def user_can_view_account(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.ACCOUNT_VIEW_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.ACCOUNT_VIEW_PERM)
     
     @staticmethod
     def user_can_change_account(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.ACCOUNT_CHANGE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.ACCOUNT_CHANGE_PERM)
 
     @staticmethod
     def user_can_add_account(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.ACCOUNT_CREATE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.ACCOUNT_CREATE_PERM)
     
     @staticmethod
     def user_can_delete_account(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.ACCOUNT_DELETE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.ACCOUNT_DELETE_PERM)
     
     ## GROUP PERMISSION
     @staticmethod
     def user_can_view_group(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.GROUP_VIEW_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.GROUP_VIEW_PERM)
     
     @staticmethod
     def user_can_change_group(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.GROUP_CHANGE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.GROUP_CHANGE_PERM)
 
     @staticmethod
     def user_can_add_group(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.GROUP_ADD_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.GROUP_ADD_PERM)
     
     @staticmethod
     def user_can_delete_group(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.GROUP_DELETE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.GROUP_DELETE_PERM)
 
     ## POLICY PERMISSION
     @staticmethod
     def user_can_view_policy(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.POLICY_VIEW_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.POLICY_VIEW_PERM)
     
     @staticmethod
     def user_can_change_policy(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.POLICY_CHANGE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.POLICY_CHANGE_PERM)
 
     @staticmethod
     def user_can_add_policy(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.POLICY_CREATE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.POLICY_CREATE_PERM)
 
     @staticmethod
     def user_can_delete_policy(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.POLICY_DELETE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.POLICY_DELETE_PERM)
 
     ## AVAILABLE SERVICE PERMISSION
 
     @staticmethod
     def user_can_change_available_service(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.AVAILABLE_SERVICE_CHANGE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.AVAILABLE_SERVICE_CHANGE_PERM)
 
     @staticmethod
     def user_can_view_available_service(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.AVAILABLE_SERVICE_VIEW_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.AVAILABLE_SERVICE_VIEW_PERM)
 
     @staticmethod
     def user_can_add_available_service(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.AVAILABLE_SERVICE_CREATE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.AVAILABLE_SERVICE_CREATE_PERM)
 
     @staticmethod
     def user_can_delete_available_service(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.AVAILABLE_SERVICE_DELETE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.AVAILABLE_SERVICE_DELETE_PERM)
 
     ## SERVICE PERMISSION
     @staticmethod
     def user_can_change_service(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.SERVICE_CHANGE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.SERVICE_CHANGE_PERM)
     
     @staticmethod
     def user_can_add_service(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.SERVICE_CREATE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.SERVICE_CREATE_PERM)
     
     @staticmethod
     def user_can_delete_service(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.SERVICE_DELETE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.SERVICE_DELETE_PERM)
 
     @staticmethod
     def user_can_view_service(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.SERVICE_VIEW_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.SERVICE_VIEW_PERM)
 
     
     ## PAYMENT PERMISSION
 
     @staticmethod
     def user_can_change_payment(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.PAYMENT_CHANGE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.PAYMENT_CHANGE_PERM)
     
     @staticmethod
     def user_can_add_payment(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.PAYMENT_CREATE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.PAYMENT_CREATE_PERM)
     
     @staticmethod
     def user_can_delete_payment(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.PAYMENT_DELETE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.PAYMENT_DELETE_PERM)
 
     @staticmethod
     def user_can_view_payment(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.PAYMENT_VIEW_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.PAYMENT_VIEW_PERM)
 
     
     ## CATEGORY PERMISSION
 
     @staticmethod
     def user_can_change_category(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.CATEGORY_CHANGE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.CATEGORY_CHANGE_PERM)
     
     @staticmethod
     def user_can_add_category(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.CATEGORY_CREATE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.CATEGORY_CREATE_PERM)
     
     @staticmethod
     def user_can_delete_category(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.CATEGORY_DELETE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.CATEGORY_DELETE_PERM)
 
     @staticmethod
     def user_can_view_category(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.CATEGORY_VIEW_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.CATEGORY_VIEW_PERM)
 
     ## TRANSFER PERMISSION
 
     @staticmethod
     def user_can_change_transfer(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.TRANSFER_CHANGE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.TRANSFER_CHANGE_PERM)
     
     @staticmethod
     def user_can_add_transfer(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.TRANSFER_CREATE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.TRANSFER_CREATE_PERM)
     
     @staticmethod
     def user_can_delete_transfer(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.TRANSFER_DELETE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.TRANSFER_DELETE_PERM)
 
     @staticmethod
     def user_can_view_transfer(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.TRANSFER_VIEW_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.TRANSFER_VIEW_PERM)
 
     
     ## VOUCHER PERMISSION
 
     @staticmethod
     def user_can_change_voucher(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.VOUCHER_CHANGE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.VOUCHER_CHANGE_PERM)
     
     @staticmethod
     def user_can_add_voucher(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.VOUCHER_CREATE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.VOUCHER_ADD_PERM)
     
     @staticmethod
     def user_can_delete_voucher(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.VOUCHER_DELETE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.VOUCHER_DELETE_PERM)
 
     @staticmethod
     def user_can_view_voucher(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.VOUCHER_VIEW_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.VOUCHER_VIEW_PERM)
 
     
     ## IDCARD PERMISSION
 
     @staticmethod
     def user_can_change_idcard(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.IDCARD_CHANGE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.IDCARD_CHANGE_PERM)
     
     @staticmethod
     def user_can_add_idcard(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.IDCARD_CREATE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.IDCARD_CREATE_PERM)
     
     @staticmethod
     def user_can_delete_idcard(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.IDCARD_DELETE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.IDCARD_DELETE_PERM)
 
     @staticmethod
     def user_can_view_idcard(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.IDCARD_VIEW_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.IDCARD_VIEW_PERM)
 
     ## CLAIM PERMISSION
 
     @staticmethod
     def user_can_change_claim(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.CASE_ISSUE_CHANGE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.CASE_ISSUE_CHANGE_PERM)
     
     @staticmethod
     def user_can_add_claim(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.CASE_ISSUE_CREATE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.CASE_ISSUE_CREATE_PERM)
     
     @staticmethod
     def user_can_delete_claim(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.CASE_ISSUE_DELETE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.CASE_ISSUE_DELETE_PERM)
 
     @staticmethod
     def user_can_view_claim(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.CASE_ISSUE_VIEW_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.CASE_ISSUE_VIEW_PERM)
     
     @staticmethod
     def user_can_close_claim(user=None):
-        return PermissionManager.user_has_perm(user=user, perm=AccessPermissions.CASE_ISSUE_CLOSE_PERM)
+        return PermissionManager.user_has_perm(user=user, perm=Constants.CASE_ISSUE_CLOSE_PERM)
