273791ac9bfbb30d10d89af3ac0910727bd4ca0c
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-12-09 00:57:53 +0100

    #4 Works on permision

--- dashboard/models.py
+++ dashboard/models.py
@@ -21,6 +21,33 @@ class AccessPermissions(models.Model):
             (Constants.VOUCHER_ADD_PERM, 'Can create Voucher'),
             (Constants.TOKEN_GENERATE_PERM, 'Can generate Token'),
             (Constants.VOUCHER_ACTIVATE_PERM, 'Can activate Voucher'),
-            (Constants.VOUCHER_RECHARGE_ACCOUNT_PERM, 'Can recharge an Account')
+            (Constants.VOUCHER_RECHARGE_ACCOUNT_PERM, 'Can recharge an Account'),
+
+            (Constants.AVAILABLE_SERVICE_VIEW_PERM, 'Dashboard Can View Available Service'),
+            (Constants.AVAILABLE_SERVICE_CHANGE_PERM, 'Dashboard Can Change Available Service'),
+            (Constants.AVAILABLE_SERVICE_ADD_PERM, 'Dashboard Can Add Available Service'),
+            (Constants.AVAILABLE_SERVICE_DELETE_PERM, 'Dashboard Can Delete Available Service'),
+
+            (Constants.SERVICE_VIEW_PERM, 'Dashboard Can View Service'),
+            (Constants.SERVICE_CHANGE_PERM, 'Dashboard Can Change Service'),
+            (Constants.SERVICE_ADD_PERM, 'Dashboard Can Add Service'),
+            (Constants.SERVICE_DELETE_PERM, 'Dashboard Can Delete Service'),
+
+            (Constants.TRANSFER_VIEW_PERM, 'Dashboard Can View Transfer'),
+            (Constants.TRANSFER_CHANGE_PERM, 'Dashboard Can Change Transfer'),
+            (Constants.TRANSFER_ADD_PERM, 'Dashboard Can Add Transfer'),
+            (Constants.TRANSFER_DELETE_PERM, 'Dashboard Can Delete Transfer'),
+
+            (Constants.CATEGORY_VIEW_PERM, 'Dashboard Can View Category'),
+            (Constants.CATEGORY_CHANGE_PERM, 'Dashboard Can Change Category'),
+            (Constants.CATEGORY_ADD_PERM, 'Dashboard Can Add Category'),
+            (Constants.CATEGORY_DELETE_PERM, 'Dashboard Can Delete Category'),
+
+            (Constants.POLICY_VIEW_PERM, 'Dashboard Can View Policy'),
+            (Constants.POLICY_CHANGE_PERM, 'Dashboard Can Change Policy'),
+            (Constants.POLICY_ADD_PERM, 'Dashboard Can Add Policy'),
+            (Constants.POLICY_DELETE_PERM, 'Dashboard Can Delete Policy'),
+
+
 
         ]
