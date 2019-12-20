120938862e1686dee9b1bdad008f0219276de19b
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2019-12-21 00:04:24 +0100

    #12 added category test

---
+++ payments/tests/category_test_data.py
@@ -0,0 +1,24 @@
+
+
+CATEGORY_CODE = 150
+
+CATEGORY_DATA = {
+    'category_name': 'TEST CATEGORY NAME',
+    'category_code': CATEGORY_CODE,
+    'is_active' : True
+}
+
+CATEGORY_DATA_NO_NAME = {
+    'category_code': CATEGORY_CODE,
+    'is_active' : True
+}
+
+CATEGORY_DATA_NO_CODE = {
+    'category_name': 'TEST CATEGORY NAME',
+    'is_active' : True
+}
+
+CATEGORY_DATA_NO_ACTIVE = {
+    'category_name': 'TEST CATEGORY NAME',
+    'category_code': CATEGORY_CODE,
+}
--- payments/tests/test_forms.py
+++ payments/tests/test_forms.py
@@ -4,7 +4,7 @@ from payments.forms import (
     PaymentForm, PolicyForm, ServiceCategoryCreationForm, ServiceCreationForm, AvailableServiceCreationForm,
     TransferForm, CaseIssueForm
 )
-from payments.tests import policy_test_data
+from payments.tests import policy_test_data, category_test_data
 
 
 
@@ -47,3 +47,31 @@ class PolicyFormTest(TestCase):
     def test_can_save_policy(self):
         form = PolicyForm(policy_test_data.POLICY_DATA)
         self.assertTrue(form.is_valid())
+
+
+class CategoryFormTest(TestCase):
+
+    def test_cannot_save_category_no_name(self):
+        form = ServiceCategoryCreationForm(category_test_data.CATEGORY_DATA_NO_NAME)
+        self.assertFalse(form.is_valid())
+
+    def test_cannot_save_category_no_code(self):
+        form = ServiceCategoryCreationForm(category_test_data.CATEGORY_DATA_NO_CODE)
+        self.assertFalse(form.is_valid())
+
+    def test_can_save_category_no_active(self):
+        form = ServiceCategoryCreationForm(category_test_data.CATEGORY_DATA_NO_ACTIVE)
+        self.assertTrue(form.is_valid())
+
+    def test_can_save_category(self):
+        form = ServiceCategoryCreationForm(category_test_data.CATEGORY_DATA_NO_ACTIVE)
+        self.assertTrue(form.is_valid())
+
+    def test_cannot_save_category_duplicate(self):
+        form = ServiceCategoryCreationForm(category_test_data.CATEGORY_DATA_NO_ACTIVE)
+        self.assertTrue(form.is_valid())
+        category = form.save()
+        form = ServiceCategoryCreationForm(category_test_data.CATEGORY_DATA_NO_ACTIVE)
+        # duplicate is now allowed.
+        # unique field is category name
+        self.assertFalse(form.is_valid()) 
