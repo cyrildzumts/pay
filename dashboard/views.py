538978c43356e4a7d7dc5226c66f6acc5582f348
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-01-25 13:37:18 +0100

    #3 minor update

--- dashboard/views.py
+++ dashboard/views.py
@@ -927,7 +927,7 @@ def policy_group_add_users(request, group_uuid=None):
             messages.add_message(request, messages.ERROR, "Policy Group %s could not updated. Invalid form".format(instance.name))
             logger.info("Edit PolicyGroupUpdateForm is not valid. Errors : %s", form.errors)
             return redirect(instance.get_dashboard_absolute_url())
-    messages.add_message(request=request, messages.ERROR, "Invalid request")
+    messages.add_message(request, messages.ERROR, "Invalid request")
     return redirect(instance.get_dashboard_absolute_url())
     """
     form = forms.PolicyGroupForm(instance=instance)
