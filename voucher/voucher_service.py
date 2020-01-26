1e4b9714480b3a7ac280476c1d40ff229e1f0217
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-01-26 16:31:21 +0100

    #3 minor update

--- voucher/voucher_service.py
+++ voucher/voucher_service.py
@@ -224,9 +224,9 @@ class VoucherService:
         }
         Account = utils.get_model("accounts", "Account")
         Recharge = utils.get_model("voucher", "Recharge")
-        recharge_account_exist = Account.objects.filter(user__username=settings.PAY_RECHARGE_USER).exist()
-        customer_account_exist = Account.objects.filter(user=customer).exist()
-        seller_account_exist = Account.objects.get(user=seller).exist()
+        recharge_account_exist = Account.objects.filter(user__username=settings.PAY_RECHARGE_USER).exists()
+        customer_account_exist = Account.objects.filter(user=customer).exists()
+        seller_account_exist = Account.objects.get(user=seller).exists()
         if not (recharge_account_exist and customer_account_exist and seller_account_exist):
             logger.info("[processing_service_request] Error : Recharge, customer ans Seller Account not found. The service request cannot be processed")
             logger.error("[processing_service_request] Error : queryset result %s instance", count)
