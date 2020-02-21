6a2b66978d619c424c271b29f552810e3bce29e4
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-02-21 21:30:06 +0100

    wip

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -14,4 +14,23 @@ function paymentCounts(){
 
 $(document).ready(function(){
 console.log("analytics ready");
+
+var ctx_payments = $('#payments-diagram');
+var ctx_transfers = $('#transfers-diagram');
+var ctx_requests = $('#payment-request-diagram');
+var ctx_users = $('#users-diagram');
+
+var payment_diagram_options = {
+    type : 'line',
+    data : {
+        labels : ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets : [{
+            label: 'Payments',
+            data: [12,48,23,45,15,16,35,26,45,48,56,66]
+        }],
+    },
+    options:{}
+};
+
+var payment_chart = new Chart(ctx_payments, payment_diagram_options);
 });
