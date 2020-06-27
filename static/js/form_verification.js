cf859ce78f3f361a6191f5f5c523e986c9614264
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-06-27 12:49:28 +0200

    update  validation transfer form

---
+++ static/js/form_verification.js
@@ -0,0 +1,36 @@
+number_regex = RegExp('^[0-9]+$');
+
+
+function validate_transfert_form(){
+    var $form = $("#transfer-form");
+    var $amount = $("#amount",$form);
+    var $recipient = $(".recipient", $form);
+    var $details = $("#details", $form);
+    var is_valid = true;
+    var submitBtn = $("#submit-btn", $form);
+    if($recipient.val().length == 0){
+        is_valid = false;
+        console.log("Recipient is missing");
+
+    }
+    if(!$amount.val().test(number_regex)){
+        is_valid = false;
+        console.log("Amount must be a number");
+    }
+    if($details.val().length == 0){
+        is_valid = false;
+        console.log("You must provide a description of the transfer");
+    }
+    if(is_valid){
+        submitBtn.removeClass(".disabled");
+    }
+}
+
+
+$(document).ready(function(){
+    var $form = $("#transfer-form");
+    $("input",$form).on('keyup change', function(){
+        validate_transfert_form();
+    });
+    $("#submit-btn", $form).addClass("disabled");
+});
