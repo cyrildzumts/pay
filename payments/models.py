72a1d0b204ff9df946dc199b88c5c5743fba6d87
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-02-22 22:46:20 +0100

    mionor update

--- payments/models.py
+++ payments/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Q
 from django.urls import reverse
 from pay import settings
 from pay import utils
