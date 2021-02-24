cf6f434d92489a0c0063161511604779a394fd6c
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-02-24 22:04:24 +0100

    minor update

--- pay/tasks.py
+++ pay/tasks.py
@@ -13,19 +13,22 @@ logger = logging.getLogger(__name__)
 
 @shared_task
 def send_mail_task(email_context=None):
-    
     # TODO : make sending email based on Django Template System.
-    if email_context is not None:
+    if email_context is not None and isinstance(email_context, dict):
         logger.debug("email_context available. Running send_mail now")
-        template_name = email_context['template_name']
-        rendered = loader.render_to_string(template_name, {'email': email_context})
-        html_message = loader.get_template(template_name=template_name).render({'email': email_context})
+        try:
+            template_name = email_context['template_name']
+        except KeyError as e:
+            logger.error(f"send_mail_task : template_name not available. Mail not send. email_context : {email_context}")
+            return
+        #message = loader.render_to_string(template_name, {'email': email_context})
+        html_message = render_to_string(template_name, email_context['context'])
         send_mail(
             email_context['title'],
-            rendered,
-            'root@local_pay.com',
+            None,
+            settings.DEFAULT_FROM_EMAIL,
             [email_context['recipient_email']],
             html_message=html_message
         )
     else:
-        logger.debug("email_context or template_name not available")
+        logger.warn(f"send_mail_task: email_context missing or is not a dict. email_context : {email_context}")
