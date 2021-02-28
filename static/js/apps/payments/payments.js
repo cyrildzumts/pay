18c6571c6d2972e9321c8b4ff9747207be47e472
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-02-28 23:29:46 +0100

    minor update

--- static/js/apps/payments/payments.js
+++ static/js/apps/payments/payments.js
@@ -1,7 +1,7 @@
 define(function(require) {
     'use strict';
     var $ = require('vendor/jquery.min');
-
+    console.log("Payment app module initialization : ", require('vendor/jquery.min'));
 
     $(function(){
         console.log("Payment app module ready");
