65581e25e2d3f05743e7c7458e34967017e27110
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-02-28 23:57:04 +0100

    mino rupdate

--- static/js/apps/payments/payments.js
+++ static/js/apps/payments/payments.js
@@ -1,7 +1,7 @@
 define(function(require) {
     'use strict';
     var $ = require('jquery');
-    console.log("Payment app module initialization : ", require('jquery').jquery);
+    console.log("Payment app module initialization : ", require('jquery')().jquery);
 
     $(function(){
         console.log("Payment app module ready");
