6b90bf982619833a5c2c13e8d101b878acd46f9b
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-03-01 12:43:06 +0100

    minor update

--- static/js/apps/payments/payments.js
+++ static/js/apps/payments/payments.js
@@ -1,8 +1,36 @@
 define(function(require) {
     'use strict';
     var $ = require('jquery');
-    console.log("Payment app module initialization : ", require('jquery')().jquery);
+    var ajax_api = require('ajax_api');
+    var PaymentManager = (function(){
+        function PaymentManager(params) {
+            
+        };
+        PaymentManager.prototype.init = function(){
 
+        };
+
+        PaymentManager.prototype.make_payment = function(data){
+
+        };
+
+        PaymentManager.prototype.make_transfer = function(data){
+
+        };
+
+        PaymentManager.prototype.recharge = function(data){
+
+        };
+
+        PaymentManager.prototype.verify = function(data){
+
+        };
+
+        PaymentManager.prototype.fetch_balance = function(){
+
+        };
+
+    });
     $(function(){
         console.log("Payment app module ready");
     });
