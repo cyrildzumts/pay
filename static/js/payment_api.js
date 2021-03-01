e1da6f1328fedbb55cbacfc2469f04524cba4ba0
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-03-01 23:28:23 +0100

    minor update

--- static/js/payment_api.js
+++ static/js/payment_api.js
@@ -1,14 +1,15 @@
-/*
+
 requirejs(['commonjs', 'apps/payments/payments'], function(common,PaymentManager){
-    //var PaymentManager =  requirejs(['apps/payments/payments']);
+    var PaymentManager =  requirejs(['payments/payments']);
     var payment_manager = new PaymentManager();
     payment_manager.init();
 });
-*/
 
+/*
 define(['apps/payments/payments', 'commonjs'], function(PaymentManager, common) {
     'use strict';
     //var PaymentManager = require('payments/payments');
     var payment_manager = new PaymentManager();
     payment_manager.init();
 });
+*/
