bbe45fab89d148a4f5a274e6c01aee7711b808a5
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-03-01 12:49:39 +0100

    mi or update

--- static/js/commonjs.js
+++ static/js/commonjs.js
@@ -1,11 +1,12 @@
 requirejs.config({
     baseUrl:'/static/js/lib',
     paths : {
-        vendor: 'vendor',
+        vendor: '/vendor',
         apps : '../apps',
         payments: '../apps/payments',
         vouchers: '../apps/vouchers',
         dashboard: '../apps/dashboard',
-        jquery : 'vendor/jquery-3.5.1'
+        jquery : 'vendor/jquery-3.5.1',
+
     }
 });
