b2c545e8b31207de0070fc6bea89d10718257fcd
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-03-01 23:14:24 +0100

    minor update

--- static/js/payment_api.js
+++ static/js/payment_api.js
@@ -1,4 +1,4 @@
-requirejs(['commonjs', 'payments/payments'], function(common, PaymentManager){
+define(['commonjs', 'payments/payments'], function(common, PaymentManager){
     var payment_manager = new PaymentManager();
     payment_manager.init();
 });
