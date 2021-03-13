93ec0997dacf3cedca5614b5de583b911f8f277e
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-03-13 01:07:23 +0100

    removed default router url

--- api/urls.py
+++ api/urls.py
@@ -6,18 +6,20 @@ from api import views, viewsets
 
 app_name = 'api'
 router = DefaultRouter()
+"""
 router.register(r'accounts', viewsets.AccountViewSet)
 router.register(r'business_accounts', viewsets.BusinessAccountViewSet)
 router.register(r'available_service', viewsets.AvailableServiceViewSet)
 router.register(r'caseissues', viewsets.CaseIssueViewSet)
 router.register(r'services', viewsets.ServiceViewSet)
-router.register(r'transfers', viewsets.TransferViewSet)
+#router.register(r'transfers', viewsets.TransferViewSet)
 router.register(r'services', viewsets.ServiceViewSet)
 router.register(r'policies', viewsets.PolicyViewSet)
 router.register(r'categories', viewsets.ServiceCategoryViewSet)
 router.register(r'vouchers', viewsets.VoucherViewSet)
 router.register(r'used_vouchers', viewsets.UsedVoucherViewSet)
 router.register(r'sold_vouchers', viewsets.SoldVoucherViewSet)
+"""
 
 partners_urlpatterns = [
     path('V1/payments/',views.dummy),
