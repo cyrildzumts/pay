79e5cb83f0b016c843096de11404d6a79b5a89d6
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-03-16 19:11:06 +0100

    minorupdate

--- core/core_service.py
+++ core/core_service.py
@@ -15,6 +15,9 @@ logger = logging.getLogger(__name__)
 def generate_reports(template_name, output_name):
     template_name = template_name or "report_content.html"
     now = datetime.datetime.now()
+    start_date = now - datetime.timedelta(days=now.day-1, hours=now.hour, minutes=now.minute, seconds=now.second)
+    end_delta = datetime.timedelta(days=1,hours=-23, minutes=-59, seconds=-59)
+    end_date = datetime.datetime(now.year, now.month +1, 1) - end_delta
     entry_list = Recharge.objects.filter(created_at__year=now.year, created_at__month=now.month)
     total = Recharge.objects.filter(created_at__year=now.year, created_at__month=now.month).aggregate(total=Sum('amount')).get('total') or 0
     context = {
@@ -25,7 +28,11 @@ def generate_reports(template_name, output_name):
         'page_size': 'letter portrait',
         'border': '',
         'entry_list' : entry_list,
-        'total' : total
+        'total' : total,
+        'CURRENCY': settings.CURRENCY,
+        'REPORT_TITLE' : 'Recharge Sumary',
+        'start_date': start_date,
+        'end_date': end_date
     }
     report_html = render_to_string(template_name, context)
     report_pdf = open(output_name, 'w+b')
