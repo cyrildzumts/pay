c7456ecc0b92fe9827ea76be2291959519d9a9b9
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-04-29 23:57:27 +0200

    minor update

---
+++ static/js/lib/styling.js
@@ -0,0 +1,22 @@
+$('.js-custom-input .input-value').on('click', function(event){
+    $(this).toggle();
+    $('.input-edit-wrapper', $(this).parent()).toggle();
+});
+
+/*
+$('.js-custom-input .js-edit').on('click', function(event){
+    $(this).parent().toggle();
+    $(this).siblings('input').toggle();
+});
+*/
+
+$('.js-custom-input input').on('keyup change', function(event){
+    var $el = $(this);
+    $el.parent().siblings('.input-value').html($el.val());
+});
+
+$('.js-custom-input .js-edit-close').on('click', function(event){
+    var $el = $(this).siblings('input');
+    $el.parent().siblings('.input-value').html($el.val());
+    $(this).parent().toggle();
+});
