05203f9baa74c09d5857e15c48f18443218065ef
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-05-01 20:52:12 +0200

    minor update

--- static/js/payment_api.js
+++ static/js/payment_api.js
@@ -1,5 +1,5 @@
 
-requirejs(['commonjs', 'apps/payments/payments', 'form_verification' , 'styling'], function(common,PaymentManager){
+requirejs(['commonjs', 'apps/payments/payments', 'form_verification' , 'lib/styling'], function(common,PaymentManager){
     var payment_manager = new PaymentManager();
     payment_manager.init();
 });
