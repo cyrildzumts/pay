25fcd5a50876830ff4870c75d4e01280a7015c44
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-05-14 23:33:09 +0200

    updating translations strings

--- payments/resources/ui_strings.py
+++ payments/resources/ui_strings.py
@@ -20,3 +20,33 @@ UI_CASHOUT_USER_INSUFFICIENT_BALANCE  = _('User has no suffient balance')
 UI_CASHOUT_AGENT_INVALID  = _('agent does not belongs to the group of Agents')
 UI_CASHOUT_USER_INVALID  = _('user must not be an agent')
 UI_INVALID_USER_REQUEST = _('Invalid user request')
+
+UI_NO_TRANSACTIONS_AVAILABLE = _('There is no transactions yet')
+UI_NO_SERVICE_AVAILABLE = _('No service available')
+
+# CATEGORIES STRINGS
+UI_CATEGORY_EDUCATION = _('Education')
+UI_CATEGORY_ELECTRICITY = _('Electricity')
+UI_CATEGORY_HEALTH = _('Health')
+UI_CATEGORY_WATER = _('Water')
+UI_CATEGORY_TRAVEL = _('Travel')
+UI_CATEGORY_ENTERTAINMENT = _('Entertainment')
+UI_CATEGORY_BUSINESS = _('Business')
+UI_CATEGORY_ONLINE_SHOPPING = _('Online Shopping')
+
+# TRANSACTIONS
+UI_MAKE_TRANSFER_LABEL = _('Send money')
+UI_MAKE_PAYMENT_LABEL = _('Make payment')
+UI_MAKE_RECHARGE_LABEL = _('Recharge')
+UI_CURRENT_BALANCE_LABEL = _('Current balance')
+UI_STATISTICS_LABEL = _('Statistics')
+
+UI_TRANSACTIONS_LABEL = _('Transactions')
+UI_AVAILABLE_SERVICES_LABEL = _('Available Services')
+UI_CATEGORIES_LABEL = _('Categories')
+UI_TRANSACTIONS_VERIFICATION_LABEL = _('Transactions verification')
+UI_RECIPIENT_LABEL = _('Recipient')
+UI_SELLER_LABEL = _('Seller')
+UI_AMOUNT_LABEL = _('Amount')
+UI_DESCRIPTION_LABEL = _('Description')
+UI_VOUCHER_CODE_LABLE = _('Voucher Code')
